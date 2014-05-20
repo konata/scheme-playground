78b60999303247b9577a45447575134141e19692
Minami <beriru@qq.com>
2014-05-20 15:17:33 +0800

    page indicate

--- number-games.scm
+++ number-games.scm
@@ -23,9 +23,21 @@
 
 
 
+(define cur-page
+  (lambda () 65))
+
+(p (cur-page))
 
 ;; the first commandment
 ;; when recurring on a list of atoms, lat , two question about it
 ;; (null? lat) and else
 ;; when recurring on a list of number, n , two question about it (zero? n) 
 ;; and else
+;;
+;;
+;; the fourth commandment
+;; always change at least one argument while recurring,it must change closer 
+;; to the termination,the changing argument must be tested int he termination
+;; condition
+;; when using cdr, termination is null?
+;; when sub1, termination is zero?
