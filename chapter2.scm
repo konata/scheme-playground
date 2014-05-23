451c1c52e2f4841a2967f5cb1ef871528c700c17
Minami <beriru@qq.com>
2014-05-23 21:36:10 +0800

    with chapter 2

--- chapter2.scm
+++ chapter2.scm
@@ -3,26 +3,52 @@
   (newline)
   )
 
+;; normal operators
+
+;(define rat cons)
+(define num car)
+(define den cdr)
+
+(define (gcd x y)
+  (if (= y 0) x (gcd y (remainder x y))))
+
 (define (rat x y)
-  ((cons x ) cons y '()))
+  (let ((divisor (gcd x y)) (att (if (> (* x y) 0) + -)))
+    (cons (att (/ x divisor)) (/ y divisor))))
 
-(define (num x)
-  (car x))
 
-(define (den x)
-  (caar x))
-
-(define (minus x)
-  (cons (- (car x)) (cdr x)))
-
-(define (add x y)
+(define (add-rat x y)
   (rat (+ (* (num x) (den y))
           (* (num y) (den x)))
        (* (den x) (den y))))
 
-(define (sub x y)
-  (add x (minus y))
-  )
+(define (sub-rat x y)
+  (rat (- (* (num x) (den y)) 
+          (* (num x) (den y)))
+       (* (den x) (den y))))
+
+(define (mul-rat x y)
+  (rat (* (num x) (den y))
+       (* (den x) (num y))))
+
+(define (div x y)
+  (rat (* (num x) (den y))
+       (* (den x) (num y))))
+
+(define (rat-eq? x y)
+  (= (* (num x) (den y)) (* (num y) (den x))))
+
+(define (print rat)
+  (newline)
+  (display (num rat))
+  (display "/")
+  (display (den rat)))
+
+;; main
+(define one-half (rat 4 -104))
+(define one-third (rat 20 -40))
+
+(print (div one-third one-half))
 
 
 
